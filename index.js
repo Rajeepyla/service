6e7aa48621782fb43dcc475831fae2dbf145552c
Rajeepyla <88529270+Rajeepyla@users.noreply.github.com>
2021-10-01 17:45:57 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,41 +1,43 @@
 const express = require('express');
-const gateway = require('fast-gateway');
-const port = 9090;
-const auth = (req,res,next)=>{
-    if(req.headers.token && req.headers.token != ''){
-     next();
+const app = express();
+const mongoose = require('mongoose')
+const port=2000;
+app.use( express.json());
+mongoose.connect('mongodb://localhost:27017/comments')
+.then(()=>{
+    console.log('connected to comments DB')
+})
+.catch((err)=>{
+    console.log('connection was failed')
+})
+const Comment = mongoose.model('Comment',new mongoose.Schema({
+    comm:{
+        type:String,
+        required:true,
+        minlength:5,
+        maxlength:50
     }
-    else{
-        res.send(JSON.stringify({status:401,message:'Authentication failed atach valid req'}))
-    }
-}
- const server = gateway({
-     routes:[{
-         prefix:'/posts',
-         target:'http://localhost:1000/'
-        
-        //  middlewares:[auth]
-     },
-     {
-        prefix:'/comments',
-        target:'http://localhost:2000/'
-    },
-    {
-        prefix:'/login',
-        target:'http://localhost:3000/'
-    },
-    {
-        prefix:'/register',
-        target:'http://localhost:4000/'
-    },
-    ]
- });
- server.get('/mygateway',(req,res)=>{
-    res.send('gateway is called')
- });
- server.start(port).then(()=>{
-    console.log(`Api gateway is listening to port ${port}`);
-}). catch((err)=>{
-    console.log(err);
-
+})); 
+app.get('/api/comment-list',async (req,res)=>{
+  const comments= await Comment.find();
+    res.send(comments);
+})
+app.post('/api/comment-list',async(req,res)=>{
+   let comment = new Comment({
+    comm: req.body.comm
+   })
+  comment = await comment.save();
+  res.send(comment);
+})
+app.put('/api/comment-list/:id',async(req,res)=>{
+    const comment = await Comment.findByIdAndUpdate(req.params.id,{ comm:req.body.comm},{ new:true});
+   
+       res.send(comment); 
+})
+app.delete('/api/comment-list/:id',async(req,res)=>{
+    const comment = await Comment.findByIdAndRemove(req.params.id)
+       res.send(comment); 
 });
+app.listen(port, ()=>{
+    console.log(`listening to port ${port}`);
+})
